285b8d9ae208350aa0f6e0b8d02e6b99aed7f11a
Calvin Donner <calvinjdonner@gmail.com>
2021-08-25 21:22:29 -0600

    added HTML rows and columns for timeblocks

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-// $("#currentDate").text(moment().format('MMMM Do YYYY'));
-
 var currentDate = moment().format('MMMM Do YYYY');
-var displayDate = document.querySelector("#currentDate");
-displayDate.append(currentDate); 
+// var displayDate = document.querySelector("#currentDate");
+// displayDate.append(currentDate); 
+
+console.log(currentDate);
+
+function displayDate() {
+    $("#currentDay").text(currentDate);
+}
+
+//if(hour > currentDate.hour) {
+    //$("future present").addClass("past")
+    //$("future").removeClass("future present");
+    
+//}
+
+//if(hour === currentDate.hour) {
+    //$("future").addClass("#present")
+    //$("future").removeClass("#future past");
+    
+//}
+
+//if(hour < currentDate.hour) {
+    //$("future").addClass("#past")
+    //$("future").removeClass("#future");
+    
+//}
