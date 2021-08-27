7bfbe105d6e2d707d99662228d15907b7e12a924
Calvin Donner <calvinjdonner@gmail.com>
2021-08-27 16:26:43 -0600

    function to save to local storage when button pushed

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 var currentDate = moment().format('MMMM Do YYYY');
-var currentHour = parseInt(moment().format('H'));
+var currentHour = parseInt(moment().format('H')); 
+
+var input9 = document.getElementById('9').innerHTML = localStorage['9'];
+var input10 = document.getElementById('10').innerHTML = localStorage['10'];
+var input11 = document.getElementById('11').innerHTML = localStorage['11'];
+var input12 = document.getElementById('12').innerHTML = localStorage['12'];
+var input13 = document.getElementById('13').innerHTML = localStorage['13'];
+var input14 = document.getElementById('14').innerHTML = localStorage['14'];
+var input15 = document.getElementById('15').innerHTML = localStorage['15'];
+var input16 = document.getElementById('16').innerHTML = localStorage['16'];
+var input17 = document.getElementById('17').innerHTML = localStorage['17'];
+
+$(".saveBtn").click(function() {
+    localStorage['9'] = document.getElementById('9').innerHTML;
+    localStorage['10'] = document.getElementById('10').innerHTML;
+    localStorage['11'] = document.getElementById('11').innerHTML;
+    localStorage['12'] = document.getElementById('12').innerHTML;
+    localStorage['13'] = document.getElementById('13').innerHTML;
+    localStorage['14'] = document.getElementById('14').innerHTML;
+    localStorage['15'] = document.getElementById('15').innerHTML;
+    localStorage['16'] = document.getElementById('16').innerHTML;
+    localStorage['17'] = document.getElementById('17').innerHTML;
+});
 
 console.log(currentDate);
 console.log(currentHour);
@@ -7,10 +29,6 @@ console.log(currentHour);
 function displayDate() {
     $("#currentDay").text(currentDate);
 }
-
-// if(myFloat < currentHour) {
-//     $(".future").last().addClass(".past").removeClass(".future");
-// }
 
 function rowUpdater() {
     $(".rowHour").each(function() {
@@ -34,22 +52,3 @@ function rowUpdater() {
 rowUpdater();
 
 displayDate();
-//if(hour > currentDate.hour) {
-    //$("future present").addClass("past")
-    //$("future").removeClass("future present");
-    
-//}
-
-//if(hour === currentDate.hour) {
-    //$("future").addClass("#present")
-    //$("future").removeClass("#future past");
-    
-//}
-
-//if(hour < currentDate.hour) {
-    //$("future").addClass("#past")
-    //$("future").removeClass("#future");
-    
-//}
-
-//how to parse the id for all timeblocks???
