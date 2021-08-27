b028b988d381d205f164f2cd599c7f798503fd6a
Calvin Donner <calvinjdonner@gmail.com>
2021-08-26 18:56:00 -0600

    added if statment format to compare current time to timeboxes

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 var currentDate = moment().format('MMMM Do YYYY');
-// var displayDate = document.querySelector("#currentDate");
-// displayDate.append(currentDate); 
+var currentHour = parseInt(moment().format('H'));
+var myFloat = parseInt("10"); //do I need to have a variable to parse every id or just parse it when I call it for comparison? 
 
 console.log(currentDate);
+console.log(currentHour);
+console.log(myFloat);
 
 function displayDate() {
-    $("#currentDay").text(currentDate);
+    $("p #currentDay").text(currentDate);
+}
+
+if(myFloat < currentHour) {
+    $(".future").last().addClass(".past").removeClass(".future");
 }
 
 //if(hour > currentDate.hour) {
@@ -25,3 +31,5 @@ function displayDate() {
     //$("future").removeClass("#future");
     
 //}
+
+//how to parse the id for all timeblocks???
