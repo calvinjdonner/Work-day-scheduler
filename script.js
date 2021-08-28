221e4b280059bb5a95e9c1b729b8b672ccce1464
Calvin Donner <calvinjdonner@gmail.com>
2021-08-28 10:38:16 -0600

    added for in loop to ensure nothing display if there is no value in local storage

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 var currentDate = moment().format('MMMM Do YYYY');
 var currentHour = parseInt(moment().format('H')); 
 
-var input9 = document.getElementById('9').innerHTML = localStorage['9'];
-var input10 = document.getElementById('10').innerHTML = localStorage['10'];
-var input11 = document.getElementById('11').innerHTML = localStorage['11'];
-var input12 = document.getElementById('12').innerHTML = localStorage['12'];
-var input13 = document.getElementById('13').innerHTML = localStorage['13'];
-var input14 = document.getElementById('14').innerHTML = localStorage['14'];
-var input15 = document.getElementById('15').innerHTML = localStorage['15'];
-var input16 = document.getElementById('16').innerHTML = localStorage['16'];
-var input17 = document.getElementById('17').innerHTML = localStorage['17'];
+for(var hour in localStorage){
+    if(localStorage.getItem(hour)) {
+       document.getElementById(hour.toString()).innerText = localStorage[hour.toString()];
+    }
+}
 
 $(".saveBtn").click(function() {
-    localStorage['9'] = document.getElementById('9').innerHTML;
-    localStorage['10'] = document.getElementById('10').innerHTML;
-    localStorage['11'] = document.getElementById('11').innerHTML;
-    localStorage['12'] = document.getElementById('12').innerHTML;
-    localStorage['13'] = document.getElementById('13').innerHTML;
-    localStorage['14'] = document.getElementById('14').innerHTML;
-    localStorage['15'] = document.getElementById('15').innerHTML;
-    localStorage['16'] = document.getElementById('16').innerHTML;
-    localStorage['17'] = document.getElementById('17').innerHTML;
+    localStorage['9'] = document.getElementById('9').innerText;
+    localStorage['10'] = document.getElementById('10').innerText;
+    localStorage['11'] = document.getElementById('11').innerText;
+    localStorage['12'] = document.getElementById('12').innerText;
+    localStorage['13'] = document.getElementById('13').innerText;
+    localStorage['14'] = document.getElementById('14').innerText;
+    localStorage['15'] = document.getElementById('15').innerText;
+    localStorage['16'] = document.getElementById('16').innerText;
+    localStorage['17'] = document.getElementById('17').innerText;
 });
 
 console.log(currentDate);
@@ -33,7 +29,6 @@ function displayDate() {
 function rowUpdater() {
     $(".rowHour").each(function() {
         var rowHours = parseInt($(this)[0].id);
-        console.log(rowHours);
 
         if(rowHours < currentHour) {
            $(this).addClass("past") 
